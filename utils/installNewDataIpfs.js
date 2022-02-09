ff9b6cfd1609c30ca8f1c8b2801fb60c0ff9ad58
antaresofficial <antaresofficial@icloud.com>
2022-02-09 18:05:00 +0500

    images install

---
+++ utils/installNewDataIpfs.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+const { PINATA_API_KEY, PINATA_API_SECRET } = process.env;
+const axios = require("axios");
+const fs = require("fs-extra");
+const recursive = require("recursive-fs");
+const FormData = require("form-data");
+const basePathConverter = require("base-path-converter");
+
+const PINATA_API_PINFILETOIPFS =
+  "https://api.pinata.cloud/pinning/pinFileToIPFS";
+
+const installNewDataIpfs = async ({ folder, data }) => {
+  try {
+    const outputPath = `./output/${folder}.json`;
+    const folderName = data;
+    const folderPath = folder;
+    const { files } = await recursive.read(folderPath);
+    if (files?.length <= 0) {
+      console.info("No files were found in folder path.");
+      return;
+    }
+    const formData = new FormData();
+    files.forEach((filePath) => {
+      formData.append("file", fs.createReadStream(filePath), {
+        filepath: basePathConverter(folderPath, filePath),
+      });
+    });
+    formData.append(
+      "pinataMetadata",
+      JSON.stringify({
+        name: folderName,
+      })
+    );
+    const {
+      data: { IpfsHash: cid },
+    } = await axios.post(PINATA_API_PINFILETOIPFS, formData, {
+      maxBodyLength: "Infinity",
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+        pinata_api_key: PINATA_API_KEY,
+        pinata_secret_api_key: PINATA_API_SECRET,
+      },
+    });
+    fs.outputJsonSync(outputPath, { [folderName]: cid });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = installNewDataIpfs;
